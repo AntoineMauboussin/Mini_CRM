7f740ba6fd8c1ffed182d7677d98f1c76aeef2fa
Antoine Mauboussin <antoine.mauboussin@livecampus.tech>
2022-08-05 10:47:52 +0200

    commit

--- minicrm.sql
+++ minicrm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2022 at 10:43 AM
+-- Generation Time: Aug 05, 2022 at 10:47 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
